ec4c475915fbd2a59dc343ac68e45b3445662563
Kitainix <dmtopskiy@gmail.com>
2023-11-13 21:06:31 +0500

    Добавьте файлы проекта.

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+unsigned long long factorial(int num) {
+	if (num <= 0)
+		return 1;
+	return factorial(num - 1) * num;
+}
+
+int fibonacci(int num) {
+	if (num <= 0)
+		return 0;
+	if (num == 1)
+		return 1;
+	return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n;
+
+	// Факториал с помощью рекурсии.
+	
+	std::cout << "Введите число: ";
+	std::cin >> n;
+	std::cout << n << "! = " << factorial(n) << std::endl;
+	
+	// Задание "Числа Фибоначчи".
+
+	std::cout << "Введите номер числа Фибоначчи: ";
+	std::cin >> n;
+	std::cout << "Число Фибоначчи №" << n << " = " << fibonacci(n) << std::endl;
+
+	return 0;
+}
